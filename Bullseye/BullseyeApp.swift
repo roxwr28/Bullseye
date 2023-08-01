fb4f7c407032e21fd1631f46d2efaec6efcd35f0
Ryan Lowe <ryanlowe23@gmail.com>
2023-07-31 19:07:17 -0500

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Ryan Lowe on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
